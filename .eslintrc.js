2ac1f31f341ffedfccf44e0f43bbf6a72fe87768
Changwan Jun <wan2land@gmail.com>
2020-06-08 11:28:46 +0900

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
     jest: true,
   },
   extends: [
-    '@stdjs',
-    '@stdjs/eslint-config/typescript',
+    'stable',
+    'stable/typescript',
   ],
   ignorePatterns: [
     'node_modules/',
